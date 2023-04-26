8f6127801bfbb647eda0372bc8f2e8bde3563880
coloniablace <bcolonia0712@gmail.com>
2023-04-26 14:44:21 +0800

    updated links

--- js/script.js
+++ js/script.js
@@ -1,52 +1,58 @@
-let navbar = document.querySelector('.header .navbar')
+let navbar = document.querySelector('.header .navbar');
 
-document.querySelector('#menu-btn').onclick = () =>{
+document.querySelector('#menu-btn').onclick = () => {
   navbar.classList.add('active');
-}
+};
 
-document.querySelector('#close-navbar').onclick = () =>{
+document.querySelector('#close-navbar').onclick = () => {
   navbar.classList.remove('active');
 };
 
 let registerBtn = document.querySelector('.account-form .register-btn');
 let loginBtn = document.querySelector('.account-form .login-btn');
 
-registerBtn.onclick = () =>{
+registerBtn.onclick = () => {
   registerBtn.classList.add('active');
   loginBtn.classList.remove('active');
-  document.querySelector('.account-form .login-form').classList.remove('active');
-  document.querySelector('.account-form .register-form').classList.add('active');
+  document
+    .querySelector('.account-form .login-form')
+    .classList.remove('active');
+  document
+    .querySelector('.account-form .register-form')
+    .classList.add('active');
 };
 
-loginBtn.onclick = () =>{
+loginBtn.onclick = () => {
   registerBtn.classList.remove('active');
   loginBtn.classList.add('active');
   document.querySelector('.account-form .login-form').classList.add('active');
-  document.querySelector('.account-form .register-form').classList.remove('active');
+  document
+    .querySelector('.account-form .register-form')
+    .classList.remove('active');
 };
 
-let accountForm = document.querySelector('.account-form')
+let accountForm = document.querySelector('.account-form');
 
-document.querySelector('#account-btn').onclick = () =>{
+document.querySelector('#account-btn').onclick = () => {
   accountForm.classList.add('active');
-}
+};
 
-document.querySelector('#close-form').onclick = () =>{
+document.querySelector('#close-form').onclick = () => {
   accountForm.classList.remove('active');
 };
 
-var swiper = new Swiper(".home-slider", {
+var swiper = new Swiper('.home-slider', {
   pagination: {
-    el: ".swiper-pagination",
-    clickable:true,
+    el: '.swiper-pagination',
+    clickable: true,
   },
-  loop:true,
-  grabCursor:true,
+  loop: true,
+  grabCursor: true,
 });
 
-var swiper = new Swiper(".home-courses-slider", {
-  loop:true,
-  grabCursor:true,
+var swiper = new Swiper('.home-courses-slider', {
+  loop: true,
+  grabCursor: true,
   spaceBetween: 20,
   breakpoints: {
     0: {
@@ -61,9 +67,9 @@ var swiper = new Swiper(".home-courses-slider", {
   },
 });
 
-var swiper = new Swiper(".teachers-slider", {
-  loop:true,
-  grabCursor:true,
+var swiper = new Swiper('.teachers-slider', {
+  loop: true,
+  grabCursor: true,
   spaceBetween: 20,
   breakpoints: {
     0: {
@@ -78,9 +84,9 @@ var swiper = new Swiper(".teachers-slider", {
   },
 });
 
-var swiper = new Swiper(".reviews-slider", {
-  loop:true,
-  grabCursor:true,
+var swiper = new Swiper('.reviews-slider', {
+  loop: true,
+  grabCursor: true,
   spaceBetween: 20,
   breakpoints: {
     0: {
@@ -95,9 +101,9 @@ var swiper = new Swiper(".reviews-slider", {
   },
 });
 
-var swiper = new Swiper(".logo-slider", {
-  loop:true,
-  grabCursor:true,
+var swiper = new Swiper('.logo-slider', {
+  loop: true,
+  grabCursor: true,
   spaceBetween: 20,
   breakpoints: {
     0: {
@@ -118,17 +124,19 @@ var swiper = new Swiper(".logo-slider", {
   },
 });
 
-let accordion = document.querySelectorAll('.faq .accordion-container .accordion');
+let accordion = document.querySelectorAll(
+  '.faq .accordion-container .accordion'
+);
 
-accordion.forEach(acco =>{
-  acco.onclick = () =>{
-    accordion.forEach(dion => dion.classList.remove('active'));
+accordion.forEach((acco) => {
+  acco.onclick = () => {
+    accordion.forEach((dion) => dion.classList.remove('active'));
     acco.classList.toggle('active');
   };
 });
 
-document.querySelector('.load-more .btn').onclick = () =>{
-  document.querySelectorAll('.courses .box-container .hide').forEach(show =>{
+document.querySelector('.load-more .btn').onclick = () => {
+  document.querySelectorAll('.courses .box-container .hide').forEach((show) => {
     show.style.display = 'block';
   });
   document.querySelector('.load-more .btn').style.display = 'none';
